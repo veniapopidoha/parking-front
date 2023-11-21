1d9198eb3651bb659399f888fdcfb39b2512dcd8
Venia <dev14@globalpartnersoft.com>
2023-11-21 16:01:29 -0500

    add reports

--- src/Pages/EmployeePage/Components/AddReport/index.js
+++ src/Pages/EmployeePage/Components/AddReport/index.js
@@ -1,26 +1,53 @@
+import React, { useState } from 'react';
+import { backLink } from '../../../../App';
 import { InputWrap } from '../../../../Components/Input/style';
-import {
-  Button,
-  Container,
-  Title,
-  Wrap,
-} from '../../../Registration/style';
+import { Button, Container, Title, Wrap } from '../../../Registration/style';
 import { StyledInput, Form, TextArea } from './style';
+import axios from 'axios';
 
 export const AddReport = () => {
+  const [plate, setPlate] = useState('');
+  const [reason, setReason] = useState('');
+  const [notes, setNotes] = useState('');
+  const [attachment, setAttachment] = useState(null);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    for (let i = 0; i < attachment.length; i++) {
+      formData.append('files', attachment[i]);
+    }
+
+    formData.append('reason', reason);
+    formData.append('plate', plate);
+    formData.append('notes', notes);
+    formData.append('buildingName', 'Building');
+
+    try {
+      const response = await axios.post(`${backLink}/add-report`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+    } catch (error) {
+      console.error('Помилка при відправці файлів:', error);
+    }
+  };
+
   return (
     <>
       <Wrap>
         <Title>Report</Title>
-        <Form onSubmit={() => {}}>
+        <Form onSubmit={handleFormSubmit}>
           <Container>
             <InputWrap>
               <StyledInput
                 // onBlur={(e) => blurHandler(e)}
-                // onChange={handleName}
+                onChange={(e) => setPlate(e.target.value)}
                 placeholder='Licence plate'
                 name='plate'
-                // value={name}
+                value={plate}
               />
             </InputWrap>
             {/* {nameEmpty && errors.nameError && <Error>{errors.nameError}</Error>} */}
@@ -29,10 +56,10 @@ export const AddReport = () => {
             <InputWrap>
               <StyledInput
                 // onBlur={(e) => blurHandler(e)}
-                // onChange={handleName}
+                onChange={(e) => setReason(e.target.value)}
                 placeholder='Reason'
                 name='reason'
-                // value={name}
+                value={reason}
               />
             </InputWrap>
             {/* {nameEmpty && errors.nameError && <Error>{errors.nameError}</Error>} */}
@@ -41,10 +68,10 @@ export const AddReport = () => {
             <InputWrap>
               <TextArea
                 // onBlur={(e) => blurHandler(e)}
-                // onChange={handleName}
+                onChange={(e) => setNotes(e.target.value)}
                 placeholder='Notes'
                 name='notes'
-                // value={name}
+                value={notes}
               />
             </InputWrap>
             {/* {nameEmpty && errors.nameError && <Error>{errors.nameError}</Error>} */}
@@ -57,9 +84,9 @@ export const AddReport = () => {
                 type='file'
                 placeholder='Attachment'
                 name='attachment'
-                accept=".jpg,.jpeg,.png,.heic"
+                accept='.jpg,.jpeg,.png,.heic'
                 multiple
-                // value={name}
+                onChange={(e) => setAttachment(e.target.files)}
               />
             </InputWrap>
             {/* {nameEmpty && errors.nameError && <Error>{errors.nameError}</Error>} */}
