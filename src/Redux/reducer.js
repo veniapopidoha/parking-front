ee4a4bf5f357c9a11ec12b3ed34515aaa47a015c
Venia <dev14@globalpartnersoft.com>
2023-12-05 18:41:32 -0500

    reducer

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -6,20 +6,20 @@ const initialState = {
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "AUTH":
+    case 'AUTH':
       return { ...state, isAuth: action.payload };
-    case "ADD_USER_DATA":
+    case 'ADD_USER_DATA':
       return {
         ...state,
         ...action.payload,
         building: action.payload.building,
       };
-    case "ADD_VISITOR_DATA":
+    case 'ADD_VISITOR_DATA':
       return {
         ...state,
         visitors: [...state.visitors, action.payload],
       };
-    case "TOGGLE_FAVORITE":
+    case 'TOGGLE_FAVORITE':
       const updatedVisitors = state.visitors.map((visitor) =>
         visitor.plate === action.payload.plate
           ? { ...visitor, favorite: !visitor.favorite }
@@ -30,8 +30,13 @@ export const reducer = (state = initialState, action) => {
         ...state,
         visitors: updatedVisitors,
       };
-    case "LOGOUT":
-      return { ...state, visitors: [], isAuth: action.payload };
+    case 'LOGOUT':
+      return {
+        ...state,
+        visitors: [],
+        isAuth: action.payload,
+        building: [],
+      };
     default:
       return state;
   }
